fdd96986ff54e8919bf7d6b2a934a7f7b667fab0
Nieves Mena Couso <nmcouso@gmail.com>
2023-05-26 20:04:09 +0100

    new database information

--- Week4/connect_db.php
+++ Week4/connect_db.php
@@ -1,6 +1,6 @@
 <?php 
     # Connect  on 'localhost' 'practice' .
-    $link = mysqli_connect('localhost','root','usbw','practice'); 
+    $link = mysqli_connect('localhost','root','lunatica2','practice'); 
     if (!$link) { 
         # Otherwise fail gracefully and explain the error. 
 	    die('Could not connect to MySQL: ' . mysqli_error()); 
